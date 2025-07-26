c84348984e65bf4514cd62dd5661cee2bd1436cf
OxyOsbourne <watkins@hungry.com>
2025-07-26 11:50:34 +0000

    cnf

---
+++ mameao/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Globals:
+    # Placeholder for global configuration/settings/state
+    RomHashStore = {}
+    Settings = {"Options": {"PlaceReport": "Yes"}}
+    BitTorrentAvailable = False
+    # TODO: Add more globals as needed
+
+globals = Globals()
